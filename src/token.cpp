fc38079b75eae36890fa4f734026093794f249ee
Jean <msv89304@stud.uni-stuttgart.de>
2017-06-09 11:51:15 +0200

    token class implemented

--- src/token.cpp
+++ src/token.cpp
@@ -1,4 +1,29 @@
 //
-// Created by frosk on 09.06.17.
+// Created by Jean on 09.06.2017.
 //
 
+#include <iostream>
+#include <vector>
+#include <string>
+#include "../include/token.h"
+
+using namespace std;
+
+/*class token:
+1     ind = 1
+2     term= "Ms."
+3     term2 = "ms."
+4     pos = "NNP"
+5     pos2 = ""
+6     head = 2
+7     type = "NMOD"*/
+
+class Token {
+public: int index, head;
+public: string word, base, pos, pos2, type;
+public:
+    Token() {};
+    Token(int indexparam, string wordparam, string baseparam, string posparam, string pos2param, int headparam,
+          string typeparam) {index = indexparam, word = wordparam, base = baseparam, pos = posparam, pos2 = pos2param,
+                             head = headparam, type = typeparam;};
+};
