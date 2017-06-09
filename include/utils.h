71b4088c6bfd3bc6ad29bf57e7248160c357f386
Jan Weßling <Jan-ek@web.de>
2017-06-09 11:43:39 +0200

    added pseudo main; added tokenize method

---
+++ include/utils.h
@@ -0,0 +1,12 @@
+//
+// Created by frosk on 09.06.17.
+//
+#include <vector>
+using namespace std;
+
+#ifndef JOHNNYDEPPENDENCY_UTILS_H
+#define JOHNNYDEPPENDENCY_UTILS_H
+
+vector<string> tokenizer(string sent, char delimiter);
+
+#endif //JOHNNYDEPPENDENCY_UTILS_H
