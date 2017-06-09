2645925e2b0981417d8cc51d9c1ee8e641c2336b
Jan Weßling <Jan-ek@web.de>
2017-06-09 09:56:00 +0200

    init repo

---
+++ cmake-build-debug/CMakeFiles/JohnnyDeppendency.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/JohnnyDeppendency.dir/src/main.cpp.o"
+  "JohnnyDeppendency.pdb"
+  "JohnnyDeppendency"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/JohnnyDeppendency.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
