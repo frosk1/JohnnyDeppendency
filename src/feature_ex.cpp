454fc12f5deb96bfa5f0399b22d54bd21adacd03
Jan Weßling <Jan-ek@web.de>
2017-06-13 18:59:02 +0200

    added arc set to features

--- src/feature_ex.cpp
+++ src/feature_ex.cpp
@@ -8,7 +8,7 @@
 #include "token.h"
 #include "utils.h"
 
-vector<string> feature_extraction(vector<vector<Token>> conf) {
+vector<string> feature_extraction(vector<vector<Token>> conf, vector<tuple<Token,Token>> arc_set){
     vector<Token> buffer = conf[0];
     vector<Token> stack = conf[1];
 
@@ -20,6 +20,11 @@ vector<string> feature_extraction(vector<vector<Token>> conf) {
     for ( Token token : buffer ) {
         feature_vector.push_back("B_" + to_string(token.index) + "_"+ token.word);
         feature_vector.push_back("B_" + to_string(token.index) + "_"+ token.pos);
+    }
+    
+    for ( tuple<Token,Token> arc : arc_set){
+        feature_vector.push_back(get<0>(arc).word + "_"+ get<1>(arc).type + "_" + get<1>(arc).word);
+        feature_vector.push_back(get<0>(arc).pos + "_"+ get<1>(arc).type + "_" + get<1>(arc).pos);
     }
 
     return feature_vector;
