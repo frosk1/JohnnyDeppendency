567f83e0270206a5d6949b7c66cbb3418fc96b10
Jan Weßling <Jan-ek@web.de>
2017-06-24 19:35:09 +0200

    added max iteration

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,13 @@
 #include "oracle.h"
 #include "Multiperceptron.h"
 #include "feature_ex.h"
-#include <unordered_map>
+#include <chrono>
+using namespace chrono;
+
 
 int main() {
+    high_resolution_clock ::time_point start = high_resolution_clock::now();
+
 
     string line;
     bool oracle_bool = true;
@@ -20,32 +24,32 @@ int main() {
     vector<string> classnames {"shift","RA","LA"};
     Multiperceptron multiperceptron(classnames);
     unordered_map<std::string,int> feature_map;
+    int max_iter = 1;
 
-    int correct = 0;
-    if (myfile.is_open())
-    {
-        while ( getline (myfile,line))
-        {
-            if (line!= ""){
+    for (int i = 0; i < max_iter; ++i) {
+        cout << "...Epoch-" << i << "..." << endl;
 
-                c++;
-                vector<string> tokens = tokenizer(line, '\t');
-                sen_tokens.push_back(tokens);
-            }
+        int correct = 0;
+        if (myfile.is_open()) {
+            while (getline(myfile, line)) {
+                if (line != "") {
 
-            else {
-                vector<Token> tokens = make_token(sen_tokens);
+                    c++;
+                    vector<string> tokens = tokenizer(line, '\t');
+                    sen_tokens.push_back(tokens);
+                } else {
+                    vector<Token> tokens = make_token(sen_tokens);
 //              conf =  [[t0] [t1,t2,t3,t4,t5]]
-                vector<vector<Token>> configuration = init_conf(tokens);
-                vector<tuple<Token,Token>> arc_set;
+                    vector<vector<Token>> configuration = init_conf(tokens);
+                    vector<tuple<Token, Token>> arc_set;
 
 //                  while buffer is not emtpy
-                    while (configuration[1].size() > 0){
+                    while (configuration[1].size() > 0) {
 
-                        if (oracle_bool){
+                        if (oracle_bool) {
 
-                            tuple<string,vector<tuple<Token,Token>>> oracle_result = oracle(configuration,
-                                                                                        arc_set, type);
+                            tuple<string, vector<tuple<Token, Token>>> oracle_result = oracle(configuration,
+                                                                                              arc_set, type);
 
                             string action;
                             action = get<0>(oracle_result);
@@ -54,8 +58,8 @@ int main() {
                             vector<string> s_feature_vector = feature_extraction(configuration, arc_set);
                             vector<int> feature_vector = feature_to_index(s_feature_vector, feature_map);
 
-                            string pred = multiperceptron.train(feature_vector,action);
-                            cout << action << ":::" << pred << endl;
+                            string pred = multiperceptron.train(feature_vector, action);
+//                            cout << action << ":::" << pred << endl;
 //                            if (pred == action){
 //                                correct++;
 //                            }
@@ -73,11 +77,8 @@ int main() {
 
                             configuration = parser(configuration, action, type);
 
-                        }
-
-                        else {
+                        } else {
                             cout << "using perceptrion";
-
 
 
                         }
@@ -85,23 +86,25 @@ int main() {
                     }
 
 
-                sen_tokens.clear();
-                cout << "finished sentence: " << sen_c << endl;
-                sen_c++;
-                c = 1;
+                    sen_tokens.clear();
+//                    cout << "finished sentence: " << sen_c << endl;
+                    sen_c++;
+                    c = 1;
 
+                }
+
+                f++;
             }
-
-            f ++;
+            myfile.close();
         }
-        myfile.close();
+        else cout << "Unable to open file";
     }
 
-    else cout << "Unable to open file";
 
 
-
-
-
+    high_resolution_clock::time_point end = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>( end - start ).count();
+    cout << "time: " << duration/1000000 << "s; ";
+    cout << (float)(duration/1000000)/(float)60 << "m";
     return 0;
 }
